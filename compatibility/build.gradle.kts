635d77cc96203d99385e58d5b57b3455b249f44a
Lumine1909 <133463833+Lumine1909@users.noreply.github.com>
2026-07-19 20:32:48 -0700

    Fix online player compatibility with FastLogin on Bukkit

--- compatibility/build.gradle.kts
+++ compatibility/build.gradle.kts
@@ -7,4 +7,5 @@ dependencies {
     implementation(project(":common"))
     compileOnly("com.viaversion:viaversion-api:5.7.1")
     compileOnly("cn.dreeam.leaf:leaf-api:1.21.11-R0.1-SNAPSHOT")
+    compileOnly(files("libs/FastLoginBukkit.jar"))
 }
--- compatibility/src/main/java/io/github/lumine1909/offlineencryptor/compat/AuthenticateCompats.java
+++ compatibility/src/main/java/io/github/lumine1909/offlineencryptor/compat/AuthenticateCompats.java
@@ -1,10 +1,13 @@
 package io.github.lumine1909.offlineencryptor.compat;
 
+import com.github.games647.fastlogin.bukkit.FastLoginBukkit;
 import io.github.lumine1909.reflexion.Method;
+import io.github.lumine1909.reflexion.exception.NotFoundException;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 import org.dreeam.leaf.event.AsyncPreAuthenticateEvent;
 
+import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.util.List;
 import java.util.UUID;
@@ -12,7 +15,7 @@ import java.util.function.BooleanSupplier;
 
 public class AuthenticateCompats {
 
-    public static final List<AuthCompat> authCompats = List.of(new LeafEvent(), new LOMCompat());
+    public static final List<AuthCompat> authCompats = List.of(new LeafEvent(), new LOMCompat(), new FastLoginCompat());
     private final BooleanSupplier disableByDefault;
 
     private AuthenticateCompats(BooleanSupplier disableByDefault) {
@@ -74,12 +77,11 @@ public class AuthenticateCompats {
         private final boolean enable;
         private Method<Boolean> method$isUserAllowed;
 
-        public LOMCompat() {
+        LOMCompat() {
             boolean enable = true;
             try {
-                Class<?> clazz = Class.forName("de.moritxius.limitedofflinemode.LimitedOfflineModePaper");
-                method$isUserAllowed = Method.of(clazz, "isUserAllowed", boolean.class, String.class);
-            } catch (ClassNotFoundException e) {
+                method$isUserAllowed = Method.of("de.moritxius.limitedofflinemode.LimitedOfflineModePaper", "isUserAllowed", boolean.class, String.class);
+            } catch (NotFoundException e) {
                 enable = false;
             }
             this.enable = enable;
@@ -94,7 +96,36 @@ public class AuthenticateCompats {
         @Override
         public boolean hasAuthentication(String username, UUID uuid, SocketAddress socketAddress) {
             Plugin plugin = Bukkit.getPluginManager().getPlugin("LimitedOfflineMode");
-            return plugin != null && !method$isUserAllowed.invoke(plugin, username);
+            return plugin != null && plugin.isEnabled() && !method$isUserAllowed.invoke(plugin, username);
+        }
+    }
+
+    static class FastLoginCompat implements AuthCompat {
+
+        private final boolean enable;
+
+        FastLoginCompat() {
+            boolean enable = true;
+            try {
+                Class.forName("com.github.games647.fastlogin.bukkit.FastLoginBukkit");
+            } catch (ClassNotFoundException e) {
+                enable = false;
+            }
+            this.enable = enable;
+        }
+
+        @Override
+        public boolean isEnable() {
+            return enable;
+        }
+
+        @Override
+        public boolean hasAuthentication(String username, UUID uuid, SocketAddress socketAddress) {
+            if (Bukkit.getPluginManager().getPlugin("FastLogin") instanceof FastLoginBukkit plugin && plugin.isEnabled()) {
+                return plugin.getSession((InetSocketAddress) socketAddress).getVerifyToken().length != 0;
+            } else {
+                return false;
+            }
         }
     }
 }
